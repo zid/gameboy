a8c20f39c4e2a2004130926a5f555d6c2b19a2a5
Spec-Chum <specchumster@gmail.com>
2020-08-06 11:39:25 +0100

    Change to SDL2

--- sdl.c
+++ sdl.c
@@ -1,97 +1,89 @@
-#include <SDL/SDL.h>
+#include <SDL2/SDL.h>
 //#include <sys/time.h>
 #include <windows.h>
 #include <stdio.h>
-static SDL_Surface *screen;
+static unsigned int* screen;
+static SDL_Window* window;
+static SDL_Renderer* renderer;
 static unsigned int frames;
-static struct timeval tv1, tv2;
+static SDL_Texture* texture;
+//static struct timeval tv1, tv2;
 
 static int button_start, button_select, button_a, button_b, button_down, button_up, button_left, button_right;
 
+const unsigned char* keystates;
+
 void sdl_init(void)
 {
-	SDL_Init(SDL_INIT_VIDEO);
-	screen = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-	SDL_WM_SetCaption("Fer is an ejit", NULL);
+	SDL_Init(SDL_INIT_EVERYTHING);
+	window = SDL_CreateWindow("Fer is an ejit",
+							SDL_WINDOWPOS_UNDEFINED,
+							SDL_WINDOWPOS_UNDEFINED,
+							640, 480,
+							SDL_WINDOW_INPUT_FOCUS);
+	renderer = SDL_CreateRenderer(window, -1, 0);
+	texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, 640, 480);
+	screen = malloc(640 * 480 * sizeof(unsigned int));
 }
 
 int sdl_update(void)
 {
+	keystates = SDL_GetKeyboardState(NULL);
+
 	SDL_Event e;
 
-	while(SDL_PollEvent(&e))
+	while (SDL_PollEvent(&e))
 	{
-		if(e.type == SDL_QUIT)
+		if (e.type == SDL_QUIT)
 			return 1;
-
-		if(e.type == SDL_KEYDOWN)
-		{
-			switch(e.key.keysym.sym)
-			{
-				case SDLK_a:
-					button_a = 1;
-				break;
-				case SDLK_s:
-					button_b = 1;
-				break;
-				case SDLK_d:
-					button_select = 1;
-				break;
-				case SDLK_f:
-					button_start = 1;
-				break;
-				case SDLK_LEFT:
-					button_left = 1;
-				break;
-				case SDLK_RIGHT:
-					button_right = 1;
-				break;
-				case SDLK_DOWN:
-					button_down = 1;
-				break;
-				case SDLK_UP:
-					button_up = 1;
-				break;
-				case SDLK_F1:
-					cpu_print_debug();
-				break;
-				case SDLK_ESCAPE:
-					return 1;
-			}
-		}
-
-		if(e.type == SDL_KEYUP)
-		{
-			switch(e.key.keysym.sym)
-			{
-				case SDLK_a:
-					button_a = 0;
-				break;
-				case SDLK_s:
-					button_b = 0;
-				break;
-				case SDLK_d:
-					button_select = 0;
-				break;
-				case SDLK_f:
-					button_start = 0;
-				break;
-				case SDLK_LEFT:
-					button_left = 0;
-				break;
-				case SDLK_RIGHT:
-					button_right = 0;
-				break;
-				case SDLK_DOWN:
-					button_down = 0;
-				break;
-				case SDLK_UP:
-					button_up = 0;
-				break;
-			}
-		}
-
 	}
+
+	if (keystates[SDL_SCANCODE_A])
+		button_a = 1;
+	else
+		button_a = 0;
+
+	if (keystates[SDL_SCANCODE_S])
+		button_b = 1;
+	else
+		button_b = 0;
+
+	if (keystates[SDL_SCANCODE_D])
+		button_select = 1;
+	else
+		button_select = 0;
+
+	if (keystates[SDL_SCANCODE_F])
+		button_start = 1;
+	else
+		button_start = 0;
+
+	if (keystates[SDL_SCANCODE_LEFT])
+		button_left = 1;
+	else
+		button_left = 0;
+
+	if (keystates[SDL_SCANCODE_RIGHT])
+		button_right = 1;
+	else
+		button_right = 0;
+
+	if (keystates[SDL_SCANCODE_UP])
+		button_up = 1;
+	else
+		button_up = 0;
+
+	if (keystates[SDL_SCANCODE_DOWN])
+		button_down = 1;
+	else
+		button_down = 0;
+
+	if (keystates[SDL_SCANCODE_F1])
+		cpu_print_debug();
+
+	if (keystates[SDL_SCANCODE_ESCAPE])
+		return 1;
+
 	return 0;
 }
 
@@ -107,7 +99,7 @@ unsigned int sdl_get_directions(void)
 
 unsigned int *sdl_get_framebuffer(void)
 {
-	return screen->pixels;
+	return screen;
 }
 
 void sdl_frame(void)
@@ -121,8 +113,12 @@ void sdl_frame(void)
 	//	gettimeofday(&tv2, NULL);
 	//	printf("Frames %d, seconds: %d, fps: %d\n", frames, tv2.tv_sec - tv1.tv_sec, frames/(tv2.tv_sec - tv1.tv_sec));
 	//}
-	Sleep(17);
-	SDL_Flip(screen);
+	//Sleep(17);
+
+	SDL_UpdateTexture(texture, NULL, screen, 640 * sizeof(unsigned int));
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
 }
 
 void sdl_quit()

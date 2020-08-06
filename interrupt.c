41728b39f68ede7769a20025fde58e16ce00257d
Peter Johnson <zidsoft@gmail.com>
2020-08-06 14:15:24 +0100

    Interrupt: Small rework now new method is tested
    Flags are combined rather than individual now.

--- interrupt.c
+++ interrupt.c
@@ -4,93 +4,66 @@
 static int enabled;
 
 /* Pending interrupt flags */
-static unsigned int vblank;
-static unsigned int lcdstat;
-static unsigned int timer;
-static unsigned int serial;
-static unsigned int joypad;
+static int interrupt_IF = 0xE0;
 
 /* Interrupt masks */
-static unsigned int vblank_masked = 1;
-static unsigned int lcdstat_masked = 1;
-static unsigned int timer_masked = 1;
-static unsigned int serial_masked = 1;
-static unsigned int joypad_masked = 1;
+static int interrupt_IE = 0;
+
+int interrupt_pending(void)
+{
+	return interrupt_IF & interrupt_IE & 0x1F;
+}
 
 void interrupt_flush(void)
 {
-	unsigned int pending = 0;
+	unsigned int pending;
 
-	if(cpu_halted() && (vblank || lcdstat || timer || serial || joypad))
-		cpu_unhalt();
+	pending = interrupt_pending();
+
+	if(!pending)
+		return;
 
 	if(!enabled)
+	{
+		if(cpu_halted())
+			cpu_unhalt();
 		return;
+	}
 
-	if(vblank && !vblank_masked)
-		pending = INTR_VBLANK;
-	if(lcdstat && !lcdstat_masked)
-		pending |= INTR_LCDSTAT;
-	if(timer && !timer_masked)
-		pending |= INTR_TIMER;
-	if(serial && !serial_masked)
-		pending |= INTR_SERIAL;
-	if(joypad && !joypad_masked)
-		pending |= INTR_JOYPAD;
+	cpu_interrupt_begin();
 
-	if(pending)
-		cpu_interrupt_begin();
-	else
-		return;
+	/* Check again here incase the above changed IF through PUSH SP (push_ei.gb) */
+	pending = interrupt_pending();
 
 	if(pending & INTR_VBLANK)
 	{
-		if(vblank_masked)
-			pending &= ~INTR_VBLANK;
-		else
-			vblank = 0;
-	}
-	if(pending & INTR_LCDSTAT)
-	{
-		if(lcdstat_masked)
-			pending &= ~INTR_LCDSTAT;
-		else
-			lcdstat = 0;
-	}
-	if(pending & INTR_TIMER)
-	{
-		if(timer_masked)
-			pending &= ~INTR_TIMER;
-		else
-			timer = 0;
-	}
-	if(pending & INTR_SERIAL)
-	{
-		if(serial_masked)
-			pending &= ~INTR_SERIAL;
-		else
-			serial = 0;
-	}
-	else if(pending & INTR_JOYPAD)
-	{
-		if(joypad_masked)
-			pending &= ~INTR_SERIAL;
-		else
-			serial = 0;
-	}
-
-	if(pending & INTR_VBLANK)
+		interrupt_IF ^= INTR_VBLANK;
 		cpu_interrupt(0x40);
+	}
 	else if(pending & INTR_LCDSTAT)
+	{
+		interrupt_IF ^= INTR_LCDSTAT;
 		cpu_interrupt(0x48);
+	}
 	else if(pending & INTR_TIMER)
+	{
+		interrupt_IF ^= INTR_TIMER;
 		cpu_interrupt(0x50);
+	}
 	else if(pending & INTR_SERIAL)
+	{
+		interrupt_IF ^= INTR_SERIAL;
 		cpu_interrupt(0x58);
+	}
 	else if(pending & INTR_JOYPAD)
+	{
+		interrupt_IF ^= INTR_JOYPAD;
 		cpu_interrupt(0x60);
+	}
 	else
+	{
 		cpu_interrupt(0x00);
+	}
 }
 
 int interrupt_enabled(void)
@@ -115,67 +88,25 @@ int interrupt_get_enabled(void)
 
 void interrupt(unsigned int n)
 {
-	/* This interrupt is now pending */
-	switch(n)
-	{
-		case INTR_VBLANK:
-			vblank = 1;
-		break;
-		case INTR_LCDSTAT:
-			lcdstat = 1;
-		break;
-		case INTR_TIMER:
-			timer = 1;
-		break;
-		case INTR_SERIAL:
-			serial = 1;
-		break;
-		case INTR_JOYPAD:
-			joypad = 1;
-		break;
-	}
+	interrupt_IF |= n;
 }
 
 unsigned char interrupt_get_IF(void)
 {
-	unsigned char mask = 0xE0;
-
-	mask |= (vblank  << 0);
-	mask |= (lcdstat << 1);
-	mask |= (timer   << 2);
-	mask |= (serial  << 3);
-	mask |= (joypad  << 4);
-
-	return mask;
+	return interrupt_IF;
 }
 
 void interrupt_set_IF(unsigned char mask)
 {
-	vblank  = !!(mask & 0x01);
-	lcdstat = !!(mask & 0x02);
-	timer   = !!(mask & 0x04);
-	serial  = !!(mask & 0x08);
-	joypad  = !!(mask & 0x10);
+	interrupt_IF = 0xE0 | mask;
 }
 
 unsigned char interrupt_get_mask(void)
 {
-	unsigned char mask = 0;
-
-	mask |= (!vblank_masked  << 0);
-	mask |= (!lcdstat_masked << 1);
-	mask |= (!timer_masked   << 2);
-	mask |= (!serial_masked  << 3);
-	mask |= (!joypad_masked  << 4);
-
-	return mask;
+	return interrupt_IE;
 }
 
 void interrupt_set_mask(unsigned char mask)
 {
-	vblank_masked  = !(mask & 0x01);
-	lcdstat_masked = !(mask & 0x02);
-	timer_masked   = !(mask & 0x04);
-	serial_masked  = !(mask & 0x08);
-	joypad_masked  = !(mask & 0x10);
+	interrupt_IE = mask;
 }
